fd67d7de6408930824f28321df83fbe725913680
rajendra <rajendra.ch@nexiilabs.com>
2017-04-13 11:07:51 +0530

    Added Calculator programm version 1.0

---
+++ calculator.py
@@ -0,0 +1,21 @@
+x = int(raw_input('Enter first number: '))
+y = int(raw_input('Enter second number: '))
+
+def addition(a, b):
+    return a + b
+
+def subtraction(a, b):
+    return abs(a - b)
+
+def multiplication(a, b):
+    return a * b
+
+def division(a,b):
+    return a / b
+
+
+print addition(x, y)
+print subtraction(x, y)
+print multiplication(x, y)
+print division(x, y)
+
