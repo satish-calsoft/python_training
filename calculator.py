6e195544285a99aa555231f7a66a20a810833a1a
kethad <kthvthdevender3@gmail.com>
2017-04-13 10:03:12 +0530

    basic_calculator

---
+++ calculator.py
@@ -0,0 +1,55 @@
+#Basic calculator
+
+
+#Store input numbers
+
+n1 = input("Enter first number: ")
+n2 = input("Enter second number: ")
+
+#Add two numbers
+
+sum = n1 + n2
+
+#Substract two nubers
+
+substract = n1 - n2
+
+#Multiply two numbers
+
+multiple = n1 * n2
+
+#Divide two numbers
+
+divide = n1 / n2
+
+#Module of two number
+
+module = n1 % n2
+
+#Exponent of two numbers
+
+exponent = n1 ** n2
+
+#Disply the sum
+
+print "The sum of %d and %d is %d" % (n1, n2, sum)
+
+#Display the substract
+
+print "The substract of %d and %d is %d" % (n1, n2, substract)
+
+#Display the multiply
+
+print "The multiply of %d and %d is %d" % (n1, n2, multiple)
+
+#Display the divide
+
+print "The divide of %d and %d is %d" % (n1, n2, divide)
+
+#Display the module
+
+print "The module of %d and %d is %d" % (n1, n2, module)
+
+#Display the exponent
+
+print "The exponent of %d and %d is %d" % (n1, n2, exponent)
