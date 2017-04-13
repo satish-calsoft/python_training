9e38f8f4bf4c66922db215cddd1b5e611ae76f16
chaitu020 <chaitu020@gmail.com>
2017-04-13 12:13:16 +0530

    for loop program

---
+++ Python_practice/Temp1.py
@@ -0,0 +1,13 @@
+#var1="Hello world"
+#print var1[:6]+'python'
+#print var1
+
+#for loops
+
+i = [1,2,3,4,5]
+
+for k in i:
+    if k%2 == 0:
+        print "%d is even" %k
+    else:
+        print "%d is not even number" %k
