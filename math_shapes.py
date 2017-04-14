460082ee8ef4383233f012440d215b48e01bc1da
rajendra <rajendra.ch@nexiilabs.com>
2017-04-14 10:48:36 +0530

    Triangle and Square
    Triangle and square using dot(s)

---
+++ math_shapes.py
@@ -0,0 +1,14 @@
+#Print triangle
+
+base_number = int(raw_input('Enter Base number: '))
+
+for x in range(base_number + 1):
+    print ' ' * (base_number -x) + '. ' * x
+
+
+
+#Print Square
+print 'Square'
+
+for x in range(base_number):
+    print '. ' * base_number
