c66462b94b4a6e2fe1e28d6d158986632fc2d1d4
Sindhu <sindhu.guturu@nexiilabs.com>
2017-04-14 10:42:13 +0530

    Modified SquarePattern File

--- SquarePattern.py
+++ SquarePattern.py
@@ -3,11 +3,14 @@ a=raw_input("Enter the character: ")
 width=4
 height=4
 
-for i in range(width/2):
-    print a*width
+print a*width
+for i in range(height-2):
+    print a*1+' '*(width-2)+a*1
+print a*width
     
-    for j in range(height-2):
-        print a*1+' '*(width-2)+a*1
+    
+    
+        
 
         
 
