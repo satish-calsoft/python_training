880e4ba78c46fa70f9c2923247ef180bc071f5b3
Jyothi <jyothi.mamidi@nexiilabs.com>
2017-04-17 19:04:01 +0530

    added hallow square

---
+++ Hallow square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+# sqaure logic
+
+size = input("Enter size:")
+inner_size = size - 2
+print ('* ' * size)
+for i in range(inner_size):
+    print ('*' + ' ' * (inner_size*2) + ' *')
+print ('* ' * size)
