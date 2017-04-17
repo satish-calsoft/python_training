78ab52aa6e6b6c226e5df28efbc6fc5a752329ff
Jyothi <jyothi.mamidi@nexiilabs.com>
2017-04-17 19:07:18 +0530

    added pythagoras

---
+++ pythagoras.py
@@ -0,0 +1,10 @@
+#! /usr/bin/python
+
+for i in range(1,101):
+    for j in range(1,101):
+       
+       for k in range(1,101):
+             if i*i  == j*j + k*k:
+             
+                 print (j,k,i)
+            
