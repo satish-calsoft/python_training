834ba3139602266563a3ca80ac1cfe3ec081a63f
suseelatest <suseela.mudunuri@nexiilabs.com>
2017-04-17 18:32:52 +0530

    this script prints the * as hallow square format

---
+++ hallow square.py
@@ -0,0 +1,12 @@
+a=input("enter value: ")
+for i in range (a):
+        if i==0 or i==a-1:
+                print ('*'+' ')*a
+        else:
+                print '*'+('  ')*(a-2)+' *'
+        
+        
+                
+    
+        
+    
