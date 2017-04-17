a6d9b57875e281ca0aced5f5df398b9500fd2702
suseelatest <suseela.mudunuri@nexiilabs.com>
2017-04-17 15:03:47 +0530

    this script displays elements into traingle pattern

---
+++ traingle pattern.py
@@ -0,0 +1,13 @@
+a=input("enter value: ")
+
+for i in range (a):
+          if i<=a:
+                    print((('*'+' ')*i).rjust(a-i)).rjust(a+i)
+                    
+                    
+                    
+           
+          
+          
+
+          
