a63934a72c7b24e80170ef8b28d52d78b56ba6da
Jyothi <jyothi.mamidi@nexiilabs.com>
2017-04-18 09:32:55 +0530

    updated pop pgm

--- pop.py
+++ pop.py
@@ -1,16 +1,17 @@
 #! /usr/bin/python
-l1 = [1,5,2,8]
-##l1.remove(l1[-1])
+l1 = [9,8,7,6,5]
+##l1.remove(l1[-1])  #1st solution to remove last index
 print l1
-l=len(l1)
-i = input("enter index to remove: ")
-for j in range(l):
-    if j==i:
-        l1.remove(j)
+i = input("enter index to remove from the list l1: ")
+for j in range(len(l1)):
     
-print "After poping the index value: ",l1
+    if j==i:
+        #l1.remove(j)
+        del l1[j]
+        
+        print "After poping the index value: ",l1 # solution to remove the value using the index.
 
-               
+         
                
         
             
