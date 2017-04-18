260409dbdaedeb9922d63c1b0b986cc1b95e1a85
ravi duda <ravikumar241@gmail.com>
2017-04-18 16:01:47 +0530

    modified square

--- square.py
+++ square.py
@@ -5,8 +5,6 @@ number = input('Enter the number = ')
 num=number*2
 print (num)*"* "
 for i in range(num-2):
-    if n>1:
-        print "* "+"  "*(num-2) + "*"
-    print (num)*"* "
-else:
-    print "Choice shoule be greaterthan 1"
+          print "* "+"  "*(num-2) + "*"
+print (num)*"* "
+
