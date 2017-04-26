f698060f55977d1b2aa5980a16a2daafeb9a61cc
Sindhu <sindhu.guturu@nexiilabs.com>
2017-04-26 15:45:24 +0530

    Triangle Pattern

--- TrianglePattern.py
+++ TrianglePattern.py
@@ -1,15 +1,26 @@
 x='*'
 ##Right Triangle
-'''for i in range(0,5):
-    print x*i'''
+#for i in range(0,5):
+    #print x*i
 a=input("Enter the value: ")
 for i in range(0,a):
     if i==0:
-        print ' '*(a-i)+'*'
+        print ' '*(a+1)+'*'
     elif i<a-1:
-        print ' '*(a-i)+'*'+' '*i*2+'*'
+        print ' '*(a-i)+'*'+' '*(i*2+1)+'*'
     elif i==a-1:
-        print ' '*(a-i)+'* '*(i+1)
+        #print ' '*(a-i)+'* '*(a+1)
+        print ' '+(('*'+' ')*(a+1))
+
+'''a=input("Enter the value: ")
+for i in range(0,a):
+    if i==0:
+        print (' ').rjust(a+1)+'*'
+    elif i<a-1:
+        print (' ').rjust(a-i)+'*'+('  ').rjust(i*2+1)+'*'
+    elif i==a-1:
+        #print ' '*(a-i)+'* '*(a+1)
+        print ' '+(('*'+' ')*(a+1))'''
         
 
     
