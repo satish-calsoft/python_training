e6d9073f8f9943e4cc41e6b53d44fd3855fcf750
suseelatest <suseela.mudunuri@nexiilabs.com>
2017-04-28 13:52:50 +0530

    this script is search word in a text file withoutusing count function

--- search word in file without using count function.py
+++ search word in file without using count function.py
@@ -1,21 +1,20 @@
-#f=open('su.txt','w')
-#f.writelines('suseela is good girl she learned python very well\njyothi,sindu and suseela learned python suseela\n suseela ')
-#f.close()
+f=open('su.txt','w')
+f.writelines('suseela is good girl she learned python very well \n jyothi and suseela learned python \n suseela hi hi.\n hi dahsdkjashjdlaksdj hdakjhdjs hi hi this suseela')
+f.close()
 f=open('su.txt','r+')
 fp=f.readlines()
 print fp
-s=raw_input('which word you want to search:')
+s=raw_input('which word you want to search in a file:')
 c=0
-
 for i in fp:
     if i.endswith('.\n'):
         a=i.replace('.\n',' ')
         fp.remove(i)
         fp.append(a)
-
+#print fp
 for i in fp:
     f1=i.split()
-    print f1
+    #print f1
     for j in f1:
         if s==j:
             c+=1
