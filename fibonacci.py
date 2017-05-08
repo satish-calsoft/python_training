256908661feeaba1643cc0faca94b6154d50c824
suseelatest <suseela.mudunuri@nexiilabs.com>
2017-05-08 19:47:49 +0530

    Add files via upload

---
+++ fibonacci.py
@@ -0,0 +1,34 @@
+l1=[]
+x=input("enter range:")
+f = 0
+s = 1
+for i in range(1,x):
+                t=f+s               
+                f=s
+                s=t 
+                #print t
+                l1.append(t)
+a=input('enter num you want to search in fibonacci:')
+if a in l1:
+                print 'its a fibonacci number'
+else:
+                print 'not a fibonacci number'
+                
+
+                
+                
+
+        
+        
+        
+   
+
+    
+    
+        
+        
+        
+        
+   
+   
+   
